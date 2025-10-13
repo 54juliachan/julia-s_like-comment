c3570ad1514b77f2126159cf0c27584ae129f968
juliachan <54juliachan@gmail.com>
2025-10-13 09:26:31 +0800

    啟動方式筆記修改

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 #喚醒終端機步驟，先後輸入以下兩串
-#cd "C:\Users\user\Desktop\碩一上\前端練習\chatgpt做的作業-改\backend"
+#cd "C:\Users\user\Desktop\碩一上\coding練習\like&comment\backend"
 #uvicorn main:app --reload
 
 from fastapi import FastAPI
